c3aefdee11d8b07b3229d98fb77265944e027489
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-09 20:42:01 -0500

    nt1 se crea proyecto

---
+++ Variables/main.py
@@ -0,0 +1,9 @@
+#VARIABLES PRIMITIVAS CON PYTHON
+nombreEstudiante ="Luisa Fernanda Correa Palacio"
+edadEstudiante=36
+estaturaEstudiante=1.57
+hinchaDelMejorEquipo=True
+#PEDIR DATOS POR TECLADO 
+comidaFavorita=input("Digita tu comida favorita:")
+#Mostrar datos en pantalla(SALIDAS)
+print(f"Hola {nombreEstudiante} tu comida favorita es {comidaFavorita}")
---
+++ datos.py
@@ -0,0 +1,4 @@
+numeroUno=input("Digita un número entero:")
+numeroDos=input("Digita el segundo número entero:")
+sumatoria=numeroUno+numeroDos
+print(f"la suma de {numeroUno}+{numeroDos}={sumatoria}")
